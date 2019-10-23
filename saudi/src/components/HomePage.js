cfe333b80875962afcd2579195146e9747142644
dantebui <senpaicaboose@gmail.com>
2019-10-23 08:54:03 -0700

    Nav Bar Styled Components Updates

--- saudi/src/components/HomePage.js
+++ saudi/src/components/HomePage.js
@@ -4,15 +4,15 @@ import Login from './Login';
 import HomeSplash from '../../public/images/HomeSplash.jpg'
 import styled from 'styled-components'
 
-const homeContainer= styled.div`
+const HomeContainer= styled.div`
 background-img: url(${HomeSplash});
 `
 const HomePage = () => {
     return(
-        <homeContainer>
+        <HomeContainer>
             <Login />
             <InventoryList />
-        </homeContainer>
+        </HomeContainer>
     )
 }
 
