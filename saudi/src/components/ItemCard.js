259888ceefb555db1b9867bc58976f8ddc348a83
Kenna <kennaalastair@gmail.com>
2019-10-23 18:34:08 -0700

    trying to getitemById in ItemCard

--- saudi/src/components/ItemCard.js
+++ saudi/src/components/ItemCard.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { getItemById } from "../actions";
+//import EditModal from './modal/EditModal';
 
 
 class ItemCard extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            search: '',
-            selectCountry: ''
-        };
-    }
 
     componentDidMount() {
         this.props.getItemById();
     }
 
     render() {
+        console.log('props', this.props);
         return (
             <>
                 <h1>hi</h1>
+                {/* <EditModal item={item} /> */}
             </>
         )
     }
--- saudi/src/components/Seller/SellerItemList.js
+++ saudi/src/components/Seller/SellerItemList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom"
 import { getItemsUserId } from "../../actions";
-import EditModal from '../modal/EditModal';
 
 
 class SellerItemList extends React.Component {
@@ -17,14 +16,13 @@ class SellerItemList extends React.Component {
             <div className="seller-item-list">
                 {this.props.items.map(item => {
                     return (
+                        <Link to={`/item/${item.id}`}>
                             <div className="item-list-card">
                                 <img className="item-photo" src={item.photo_url} alt="item" />
-                                <Link to={`/item/${item.id}`}>
                                     <h3>{item.name}<span>{item.price}</span></h3>
-                                </Link>
                                 <p>{item.description}</p>
-                                <EditModal />
                             </div>
+                        </Link>
                     )
                 })}
             </div>
