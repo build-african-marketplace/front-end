cb910b75fb64305f0796ce82ce6641a2ec7a4c1d
Kenna <kennaalastair@gmail.com>
2019-10-24 09:32:04 -0700

    addItem action and reducer

--- saudi/src/actions/index.js
+++ saudi/src/actions/index.js
@@ -76,6 +76,24 @@ export const getItemById = (id) => dispatch => {
         })
 }
 
+export const ADD_ITEM = 'ADD_ITEM';
+export const ADD_ITEM_SUCCESS = 'ADD_ITEM_SUCCESS';
+export const ADD_ITEM_FAILURE = 'ADD_ITEM_FAILURE';
+
+export const addItem = item => dispatch => {
+    dispatch({ type: ADD_ITEM });
+
+    axiosWithAuth()
+        .post(`/items`, item)
+        .then(res => {
+            dispatch({ type: ADD_ITEM_SUCCESS, payload: res.data });
+        })
+        .catch(err => {
+            console.error(err);
+            dispatch({ type: ADD_ITEM_FAILURE, payload: err });
+        })
+}
+
 // export const ModalActionTypes = keyMirror({
 //     HIDE_MODAL: null,
 //     SHOW_MODAL: null,
