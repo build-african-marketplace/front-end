de090d5dd5d1b6619a6e96deb2afca8da5b10149
Jnmendza <jonatmendoza@gmail.com>
2019-10-24 15:35:56 -0700

    footer and imgs for styling

--- saudi/src/components/Footer.js
+++ saudi/src/components/Footer.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Logo from '../images/Logo_white.png'
+import styled from 'styled-components';
+
+const FooterLogo = styled.img`
+    width: 15%;
+    height: 15%;
+`;
+
+const FooterContainer = styled.div`
+    background: #1D2C3C;
+    color: white;
+    display: flex;
+`;
+
+const Info = styled.div`
+    width: 20%;
+    justify-content: flex-end;
+`;
 
 
-export const Footer = () => {
+const Footer = () => {
     return (
-        <div className='footer'>
-            <img src="" />
+        <>
+        <FooterContainer>
+        <FooterLogo src={Logo} />
+        <Info>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <label>Follow Us</label>
-        </div>
+        </Info>
+        </FooterContainer>
+        </>
     )
 }
+export default Footer;
 
